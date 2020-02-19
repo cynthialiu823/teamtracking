084a1957af5083e01cc64bf5c5dba61e27b080dd
neurojojo <joemesz@gmail.com>
2020-02-19 12:23:30 -0500

    First commit

---
+++ getting_started_detection.m
@@ -0,0 +1,10 @@
+movieParam=struct();
+detectionParam=struct();
+saveResults=0;
+verbose=0;
+
+movieParam.imageDir = 
+movieParam.filenameBase = 
+movieParam.firstImageNum =
+movieParam.lastImageNum =
+movieParam.digits4Enum =
